60399e5649d6355934f2ae0fda3f625f2bc62a9b
sickdon <donatodisapia@gmail.com>
2016-12-15 14:30:51 +0100

    ControllerAllenatore

--- src/AppBundle/comunicazione/ControllerChatAllenatore.php
+++ src/AppBundle/comunicazione/ControllerChatAllenatore.php
@@ -33,66 +33,130 @@ class ControllerChatAllenatore extends Controller
 
     }
 
+    /**
+     * @Route("/inviaRichiamoMulta")
+     * @Method("POST")
+     */
     public function inviaRichiamoMulta(Request $richiesta){
 
     }
 
+    /**
+     * @Route("/inviaRichiamoAvvertimento")
+     * @Method("POST")
+     */
     public function inviaRichiamoAvvertimento(Request $richiesta){
 
     }
 
+    /**
+     * @Route("/inviaRichiamoDieta")
+     * @Method("POST")
+     */
     public function inviaRichiamoDieta(Request $richiesta){
 
     }
 
+    /**
+     * @Route("/inviaRichiamoAllenamento")
+     * @Method("POST")
+     */
     public function inviaRichiamoAllenamento(Request $richiesta){
 
     }
 
+    /**
+     * @Route("/ottieniMessaggioForm")
+     * @Method("GET")
+     */
     public function ottieniMessaggioForm(){
 
     }
 
+    /**
+     * @Route("/ottieniMessaggioVoceForm")
+     * @Method("GET")
+     */
     public function ottieniMessaggioVoceForm(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoMultaForm")
+     * @Method("GET")
+     */
     public function ottieniRichiamoMultaForm(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoAvvertimentoForm")
+     * @Method("GET")
+     */
     public function ottieniRichiamoAvvertimentoForm(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoDietaForm")
+     * @Method("GET")
+     */
     public function ottieniRichiamoDietaForm(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoAllenamentoForm")
+     * @Method("GET")
+     */
     public function ottieniRichiamoAllenamentoForm(){
 
     }
 
+    /**
+     * @Route("/ottieniMessaggioView")
+     * @Method("GET")
+     */
     public function ottieniMessaggioView(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoVoceView")
+     * @Method("GET")
+     */
     public function ottieniMessaggioVoceView(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoMultaView")
+     * @Method("GET")
+     */
     public function ottieniRichiamoMultaView(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoAvvertimentoView")
+     * @Method("GET")
+     */
     public function ottieniRichiamoAvvertimentoView(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoDietaView")
+     * @Method("GET")
+     */
     public function ottieniRichiamoDietaView(){
 
     }
 
+    /**
+     * @Route("/ottieniRichiamoAllenamentoView")
+     * @Method("GET")
+     */
     public function ottieniRichiamoAllenamentoView(){
 
     }
