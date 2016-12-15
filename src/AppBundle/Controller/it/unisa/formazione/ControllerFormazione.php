3997d8f53f6ffd35a8fe80c23211df60ba2a91f1
Luigi Durso <alduin.93y@gmail.com>
2016-12-15 09:47:20 +0100

    add formazione controller

---
+++ src/AppBundle/Controller/it/unisa/formazione/ControllerFormazione.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: luigidurso
+ * Date: 15/12/16
+ * Time: 09:37
+ */
+
+
+
+namespace AppBundle\Controller\it\unisa\formazione;
+
+class ControllerFormazione extends Controller
+{
+
+
+}
