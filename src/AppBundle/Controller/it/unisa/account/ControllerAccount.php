2dcaa321a0af1ee7612356beb0485d6644af5776
user\useruser <raffaeleceruso@gmail.com>
2016-12-22 10:32:54 +0100

    Corretto Controller

--- src/AppBundle/Controller/it/unisa/account/ControllerAccount.php
+++ src/AppBundle/Controller/it/unisa/account/ControllerAccount.php
@@ -40,9 +40,9 @@ class ControllerAccount extends Controller
     public function aggiungiAccountForm($attore)
     {
         if($attore=="staff_allenatore_tifoso")
-                return $this->render("account/formaggiungiAccountATS.html.twig");/*vista non completa*/
+            return $this->render("account/formaggiungiAccountATS.html.twig");/*vista non completa*/
         else
-            return new Response("da fare");
+            return $this->render("account/formAggiungiAccounGiocatore.html.twig");/*vista non completa*/
 
     }
     /**
@@ -55,9 +55,9 @@ class ControllerAccount extends Controller
     */
     public function aggiungiAccount(Request $r,$attore)
     {
-
+        $g = new GestoreAccount();
         if($attore=="staff_allenatore_tifoso"){
-            $g = new GestoreAccount();
+
             $a = new Account($r->request->get("u"),
                 $r->request->get("p"),
                 $r->request->get("s"),
@@ -66,10 +66,6 @@ class ControllerAccount extends Controller
                 $r->request->get("do"), $r->request->get("i"),
                 $r->request->get("pr"), $r->request->get("t"),
                 $r->request->get("im"), $r->request->get("tipo"));
-            $validator=$this->get("validator");
-            $e=$validator->validate($a);
-            if(count($e)>0)
-                return new Response((string)$e,404);
             try {
                 $g->aggiungiAccount_A_T_S($a);
                 return new Response("inserimento andato a buon fine");
@@ -79,8 +75,8 @@ class ControllerAccount extends Controller
         }
         else
             if($attore=="calciatore") {
-                $g = new GestoreAccount();
-                $a = new Calciatore($r->request->get("u"),
+
+                $a = new AccountCalciatore($r->request->get("u"),
                     $r->request->get("p"),
                     $r->request->get("s"),
                     $r->request->get("e"), $r->request->get("n"),
@@ -94,8 +90,8 @@ class ControllerAccount extends Controller
                 } catch (\Exception $e) {
                     return new Response($e->getMessage(), 404);
                 }
-        }
-        else return new Response("la rotta non esiste",404);
+            }
+            else return new Response("la rotta non esiste",404);
 
     }
 
@@ -123,15 +119,20 @@ class ControllerAccount extends Controller
         $g = new GestoreAccount();
         if($attore=="staff_allenatore_tifoso"){
             try {
-                $a = $g->ricercaAccount_A_T_S($username);
-                return new Response("ACC:" . $a->getUsernameCodiceContratto() . "appartiene alla squadra" . $a->getSquadra());
+                $ast = $g->ricercaAccount_A_T_S($username);
+                return new Response("ACC:" . $ast->getUsernameCodiceContratto() . "appartiene alla squadra" . $ast->getSquadra());
             } catch (\Exception $e) {
                 return new Response($e->getMessage(), 404);
             }
         }
-        else return new Response("da fare",404);
-
-
+        else {
+            try {
+                $ag = $g->ricercaAccount_G($username);
+                return new Response("ACC:" . $ag->getUsernameCodiceContratto() . "appartiene alla squadra" . $ag->getSquadra());
+            }catch(\Exception $e) {
+                return new Response($e->getMessage(), 404);
+            }
+        }
     }
 
     /**
@@ -146,7 +147,7 @@ class ControllerAccount extends Controller
     {
         if($attore=="staff_allenatore_tifoso"){
             /*il tipo e la squadra non possono essere modificati quindi non glieli inviamo proprio non devono */
-            $g = new GestoreAccount();
+            $gats = new GestoreAccount();
             $a = new Account($r->request->get("u"),
                 $r->request->get("p"), "",
                 $r->request->get("e"), $r->request->get("n"),
@@ -156,14 +157,34 @@ class ControllerAccount extends Controller
                 $r->request->get("im"), "");
 
             try {
-                $g->modificaAccount_A_T_S($r->request->get("u"), $a);
+                $gats->modificaAccount_A_T_S($r->request->get("u"), $a);
                 return new Response("modifica andata a buon fine");
             } catch (\Exception $e) {
                 return new Response($e->getMessage(), 404);
             }
         }
-        else return new Response("ancora da fare",404);
+        else if($attore=="giocatore"){
+            $gg = new GestoreAccount();
 
+
+
+            /*il tipo e la squadra non possono essere modificati quindi non glieli inviamo proprio non devono */
+
+            $a = new AccountCalciatore($r->request->get("u"),
+                $r->request->get("p"), "",
+                $r->request->get("e"), $r->request->get("n"),
+                $r->request->get("c"), $r->request->get("d"),
+                $r->request->get("do"), $r->request->get("i"),
+                $r->request->get("pr"), $r->request->get("t"),
+                $r->request->get("im"), $r->request->get("nazionalità"));
+
+            try {
+                $gg->modificaAccount_G($r->request->get("u"), $a);
+                return new Response("modifica andata a buon fine");
+            } catch (\Exception $e) {
+                return new Response($e->getMessage(), 404);
+            }
+        }
 
     }
 
@@ -180,13 +201,22 @@ class ControllerAccount extends Controller
         if($attore=="staff_allenatore_tifoso"){
             $g = new GestoreAccount();
             try {
-                $a = $g->eliminaAccount_A_T_S($username);
+                $g->eliminaAccount_A_T_S($username);
+                return new Response("ACC:" . $username . "eliminato");
+            } catch (\Exception $e){
+                return new Response($e->getMessage(), 404);
+            }
+        }
+        else if($attore=="calciatore") {
+            $g = new GestoreAccount();
+            try {
+                $g->eliminaAccount_G($username);
                 return new Response("ACC:" . $username . "eliminato");
             } catch (\Exception $e) {
                 return new Response($e->getMessage(), 404);
             }
         }
-        else return new Response("ancora da fare",404);
+
 
 
     }
