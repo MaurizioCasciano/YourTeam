8b1e622220bedd222d7bd96ae87d77ef4c07716e
user\useruser <raffaeleceruso@gmail.com>
2017-01-27 17:14:24 +0100

    Miglioramento controlli registrazione

--- src/AppBundle/Controller/it/unisa/account/ControllerAccount.php
+++ src/AppBundle/Controller/it/unisa/account/ControllerAccount.php
@@ -61,6 +61,7 @@ class ControllerAccount extends Controller
     {
 
 
+
         //  $autenticazione= GestoreAutenticazione::getInstance();
         //  if ($autenticazione->check($r->get("_route"))) {
 
